25ac904d0ed48a93f681b4428e9895c578c69371
Alexander <sashasakovich06@gmail.com>
2026-03-22 12:48:44 +0300

    Add necessary gems
    -Add rubocop gems
    -Add test gems
    -Configure added gems

---
+++ spec/support/simplecov.rb
@@ -0,0 +1,15 @@
+require 'simplecov'
+
+SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+SimpleCov.start 'rails' do
+  enable_coverage_for_eval
+  add_filter(%r{^/spec/})
+  add_filter('/app/channels/')
+  add_filter('/app/jobs/')
+  add_filter('/app/mailers/')
+  add_filter('application_record.rb')
+  add_filter('application_job.rb')
+  add_filter('application_mailer.rb')
+  add_filter('application_cable/')
+  enable_coverage(:branch)
+end
